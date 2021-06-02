f892d102394adee52704b222d44f2f57c1f09eba
肖文清 <2487732209@qq.com>
2021-06-02 19:06:02 +0800

    new product UI

---
+++ src/pages/fall2021.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>2021 F/W COLLECTION</h3>
+    <vs-row>
+      <vs-col w="4">
+          <img src="../assets/0.jpg" width="100%" height="auto" alt="">
+      </vs-col>
+      <vs-col w="4">
+          <img src="../assets/1.jpg" width="100%" height="auto" alt="">
+      </vs-col>
+      <vs-col w="4">
+          <img src="../assets/2.jpg" width="100%" height="auto" alt="">
+      </vs-col>
+    </vs-row>
+    <vs-row>
+      <vs-col w="6">
+          <img src="../assets/3.jpg" width="100%" height="auto" alt="">
+      </vs-col>
+      <vs-col w="6">
+          <img src="../assets/4.jpg" width="100%" height="auto" alt="">
+      </vs-col>
+    </vs-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FALL2021",
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
